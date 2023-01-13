742ecc1d16320658924a0cf2860072308939ee0c
Abhishek <abhishekbhikule76@gmail.com>
2023-01-13 19:54:21 +0530

    Form added

--- textLoader.js
+++ textLoader.js
@@ -1,5 +1,18 @@
 const text = document.querySelector(".sec-text");
 
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+const hiddenElements = document.querySelectorAll(".hidden ");
+hiddenElements.forEach((el) => observer.observe(el));
+
 function textLoader() {
   setTimeout(() => {
     text.textContent = "Abhishek";
